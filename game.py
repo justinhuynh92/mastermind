110b46465fe1f88ffc3a8cbcd2d371427c8b71c1
Justin Huynh <justinhuynh639@gmail.com>
2023-10-12 23:45:58 -0700

    started mastermind

---
+++ game.py
@@ -0,0 +1,10 @@
+import random
+
+# set colors and 10 tries
+COLORS = ['R', 'G', 'B', 'Y', 'W', 'O']
+TRIES = 10
+CODE_LENGTH = 4
+
+# define function that's going to generate the code for us
+def generate_code():
+    
