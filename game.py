2e04d7d3a3f440b5a5936e6cb9580f3fe9fb6add
Justin Huynh <justinhuynh639@gmail.com>
2023-10-13 10:33:58 -0700

    checked colors pos

--- game.py
+++ game.py
@@ -44,3 +44,14 @@ def check_code(guess, real_code):
     # keep track of colors that are in and not in the correct position
     correct_pos = 0
     incorrect_pos = 0
+
+    for color in real_code:
+        if color not in color_counts:
+            color_counts[color] = 0
+        color_counts[color] += 1
+
+    #find colors that are in the correct position
+    for guess_color, real_color in zip(guess, real_code):
+        if guess_color == real_color:
+            correct_pos += 1
+            color_counts[guess_color] -= 1
