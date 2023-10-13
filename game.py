bd7f51e11e45ce2c503aac1e18850bb6754e53c4
Justin Huynh <justinhuynh639@gmail.com>
2023-10-13 10:38:02 -0700

    changes

--- game.py
+++ game.py
@@ -55,3 +55,11 @@ def check_code(guess, real_code):
         if guess_color == real_color:
             correct_pos += 1
             color_counts[guess_color] -= 1
+
+    for guess_color, real_color in zip(guess, real_code):
+        if guess_color in color_counts and color_counts[guess_color] > 0:
+            incorrect_pos += 1
+            color_counts[guess_color] -= 1
+
+    return correct_pos, incorrect_pos
+
