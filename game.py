9cdc88ac4c5922066899a13f7e022de32fab605e
Justin Huynh <justinhuynh639@gmail.com>
2023-10-12 23:48:58 -0700

    choose colors 4 times

--- game.py
+++ game.py
@@ -7,4 +7,12 @@ CODE_LENGTH = 4
 
 # define function that's going to generate the code for us
 def generate_code():
-    
+    code = []
+
+    # insert 4 random colors into list
+    for _ in range(CODE_LENGTH):
+        # select random element from colors
+        color = random.choice(COLORS)
+        code.append(color)
+
+    return code
